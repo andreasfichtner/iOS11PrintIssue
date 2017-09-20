3f28be584c7536c4e953906e6160a3e6640828d6
Andreas Fichtner <a.fichtner@iprax-systems.com>
2017-09-20 18:56:20 +0200

    Initial Commit

---
+++ iOS11PrintIssues/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS11PrintIssues
+//
+//  Created by Andreas Fichtner on 20.09.17.
+//  Copyright © 2017 iPrax Systems GmbH & Co KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS11PrintIssues/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS11PrintIssues
+//
+//  Created by Andreas Fichtner on 20.09.17.
+//  Copyright © 2017 iPrax Systems GmbH & Co KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
